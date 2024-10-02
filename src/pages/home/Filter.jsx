2c84cc4c48127ab067220f834c22b549603cf539
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-10-02 08:26:25 +0200

    adjust styles

--- src/pages/home/Filter.jsx
+++ src/pages/home/Filter.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import "./filter.css";
 import { Tag } from "../../components/tag/Tag";
 
 export const Filter = () => {
   return (
-    <div>
+    <div className="tag-list">
       <Tag>Social</Tag>
+      <Tag>Educació</Tag>
+      <Tag>Caritat</Tag>
+      <Tag>Cuinar</Tag>
+      {/* <Tag>Relaxació</Tag>
+      <Tag>Treball intens</Tag> */}
     </div>
   );
 };
