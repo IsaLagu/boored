d2f48dd5605cadd85d86ed3014ae52094d3d3f0a
Isa Afonso <126961475+IsaLagu@users.noreply.github.com>
2024-10-02 17:55:32 +0200

    add handleresize for tags

--- src/pages/home/Filter.jsx
+++ src/pages/home/Filter.jsx
@@ -1,15 +1,34 @@
+import React, { useState, useEffect } from "react";
 import "./filter.css";
 import { Tag } from "../../components/tag/Tag";
 
 export const Filter = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 600); // 768px es un punto de quiebre común para móvil
+  };
+
+  useEffect(() => {
+    handleResize(); // Verifica el tamaño inicial
+    window.addEventListener("resize", handleResize); // Escucha cambios en el tamaño de la ventana
+    return () => {
+      window.removeEventListener("resize", handleResize); // Limpia el evento
+    };
+  }, []);
+
   return (
     <div className="tag-list">
       <Tag>Social</Tag>
       <Tag>Educació</Tag>
       <Tag>Caritat</Tag>
       <Tag>Cuinar</Tag>
-      {/* <Tag>Relaxació</Tag>
-      <Tag>Treball intens</Tag> */}
+      {!isMobile && (
+        <>
+          <Tag>Relaxació</Tag>
+          <Tag>Treball intens</Tag>
+        </>
+      )}
     </div>
   );
 };
